7a2d9771f820d4433af9d07d437ea7edccf049a2
Lucas Villela Neder Issa <issa@dcc.ufmg.br>
2017-07-07 15:47:08 -0300

    Removing commented code.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,2 @@
-// TODO: Export temporário. Teria que ver como importar diretamente do folder.
-// export { default as Samples } from './samples/Samples';
-
 export { ReduxFormFieldSimpleReactDropzone } from './ui/ReduxFormFieldSimpleReactDropzone';
-
 export { SimpleReactDropzone } from './ui/SimpleReactDropzone';
